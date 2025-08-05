b2ef585823ca921a4c2b08acbc61a44ed65f7ebc
kottybarn3567 <kaualopes3567@gmail.com>
2025-08-05 00:25:09 +0000

    adicionando as o calculo dentro das novas variaveis para densidade e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,14 +23,14 @@ int main() {
 
     // DECLARACAO DOS TIPOS DE DADOS PARA O CARD 1 E 2
     char pais1[30], cidade1[30], codigo1[3];
-    float area1, pib1;
-    int populacao1, ponts_turist1;
+    float populacao1, area1, pib1, dens_pop1, cap_pib1; // MUDEI AS POPULACOES PARA float
+    int ponts_turist1;
 
-    char pais2[30], cidade2[30], codigo2[2];
-    float area2, pib2;
-    int populacao2, ponts_turist2;
+    char pais2[30], cidade2[30], codigo2[3];
+    float populacao2, area2, pib2, dens_pop2, cap_pib2;
+    int ponts_turist2;
     
-    printf("=================================================\n");
+    printf("=================================================\n\n");
     printf("Bem vindo ao Cadastro de cartas\n");
     printf("\n");
 
@@ -40,13 +40,13 @@ int main() {
     scanf(" %s", cidade1);
 
     printf("Nome do Pais da Cidade1: ");
-    scanf(" %s", &pais1);
+    scanf(" %s", pais1);
 
     printf("Estado - UF: ");
     scanf(" %s", codigo1);
 
     printf("Populacao da Cidade1: ");
-    scanf(" %d", &populacao1);
+    scanf(" %f", &populacao1);
 
     printf("Area da Cidade1: ");
     scanf(" %f", &area1);
@@ -57,21 +57,27 @@ int main() {
     printf("Pontuacao Turistica: ");
     scanf(" %d", &ponts_turist1);
 
+    // CRIANDO AS VARIAVEIS DE DENSIDADE E PIB PER CAPTA DO CARD 1
+    dens_pop1 = populacao1 / area1;
+    cap_pib1 = pib1 / populacao1;
 
     printf("=================================================\n");
     
     printf("CIDADE1: %s \n", cidade1);
     printf("PAIS DA CIDADE1: %s \n", pais1);
     printf("UF1: %s \n", codigo1);
-    printf("POLULACAO1: %d milhoes \n", populacao1);
+    printf("POLULACAO1: %.1f \n", populacao1);
     printf("AREA1: %.1f km2 \n", area1);
-    printf("PIB1: %f milhoes \n", pib1);
+    printf("PIB1: R$ %.1f \n", pib1);
     printf("TURISMO: %d pontos \n", ponts_turist1);
+    printf("DENSIDADE: %.1f \n", dens_pop1);
+    printf("PIB PERCAPITA: R$ %.3f \n", cap_pib1);
 
     printf("=================================================\n");
 
 
 
+    // LEITURA DOS DADOS DO PRIMEIRO CARD 2
     printf("=================================================\n");
 
     printf("Nome da Cidade2: ");
@@ -84,7 +90,7 @@ int main() {
     scanf(" %s", codigo2);
 
     printf("Populacao da Cidade2: ");
-    scanf(" %d", &populacao2);
+    scanf(" %f", &populacao2);
 
     printf("Area da Cidade2: ");
     scanf(" %f", &area2);
@@ -95,9 +101,13 @@ int main() {
     printf("Pontuacao Turistica: ");
     scanf(" %d", &ponts_turist2);
 
+    // CRIANDO AS VARIAVEIS DE DENSIDADE E PIB PER CAPTA DO CARD 2
+    dens_pop2 = populacao2 / area2;
+    cap_pib2 = pib2 / populacao2;
+
     // FIZ EM UMA UNICA LINHA PARA FICAR DIFERENTE
     printf("=================================================\n");
-    printf("CIDADE1: %s\nPAIS DA CIDADE1: %s\nUF1: %s\nPOLULACAO1: %d milhoes\nAREA1: %.1f km2\nPIB1: %f milhoes\nTURISMO: %d pontos\n", cidade2, pais2, codigo2, populacao2, area2, pib2, ponts_turist2);
+    printf("CIDADE2: %s\nPAIS DA CIDADE2: %s\nUF2: %s\nPOLULACAO2: %.1f \nAREA2: %.1f km2\nPIB2: R$ %.1f \nTURISMO: %d pontos\nDENSIDADE: %.1f\nPIB PERCAPITA: R$ %.3f\n", cidade2, pais2, codigo2, populacao2, area2, pib2, ponts_turist2, dens_pop2, cap_pib2);
     printf("=================================================\n");
 
     return 0;
